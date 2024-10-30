f869716f69396187c4de9577785fbfe18b3fab6c
Troy <20098362@mail.wit.ie>
2024-10-30 20:40:27 +0000

    add now playing method to api

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -118,3 +118,19 @@ export const getPopular = () => {
             throw error
         });
 };
+
+export const getNowPlaying = () => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    ).then((response) => {
+        if (!response.ok) {
+            return response.json().then((error) => {
+                throw new Error(error.status_message || "Something went wrong");
+            });
+        }
+        return response.json();
+    })
+        .catch((error) => {
+            throw error
+        });
+};
