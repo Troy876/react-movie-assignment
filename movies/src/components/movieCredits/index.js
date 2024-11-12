91af7472f844f9b3f7883626ce1c296b66636a6b
Troy <20098362@mail.wit.ie>
2024-11-12 22:38:45 +0000

    add credit drawer data

--- movies/src/components/movieCredits/index.js
+++ movies/src/components/movieCredits/index.js
@@ -1,17 +1,65 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Link } from "react-router-dom";
+import { getMovieCredits } from "../../api/tmdb-api";
+import { excerpt } from "../../util";
+import { useQuery } from "react-query";
+import Spinner from '../spinner'
 
-const MovieCredits = ({ credits }) => {
-    return (
-        <>
-            <Typography variant="h5" component="h3">
-                Cast: {credits.crew}
-            </Typography>
-
-            <Typography variant="h6" component="p">
-                Crew: {credits.crew}
-            </Typography>
-        </>
+export default function MovieReviews({ movie }) {
+    const { data, error, isLoading, isError } = useQuery(
+        ["credits", { id: movie.id }],
+        getMovieCredits
     );
-};
-export default MovieCredits
+
+    if (isLoading) {
+        return <Spinner />;
+    }
+
+    if (isError) {
+        return <h1>{error.message}</h1>;
+    }
+
+    const credits = data?.cast || [];
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 550 }} aria-label="credits table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell >Role</TableCell>
+                        <TableCell align="center">Name</TableCell>
+                        <TableCell align="right">More</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {credits?.map((r) => (
+                        <TableRow key={r.id}>
+                            <TableCell component="th" scope="row">
+                                {r.name}
+                            </TableCell>
+                            <TableCell >{excerpt(r.character)}</TableCell>
+                            <TableCell >
+                                <Link
+                                    to={`/credits/${r.id}`}
+                                    state={{
+                                        credits: r,
+                                        movie: movie,
+                                    }}
+                                >
+                                    Full Review
+                                </Link>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
