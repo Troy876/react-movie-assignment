39799dcf6a84612d49d933d99726a1c8e660f600
Troy <20098362@mail.wit.ie>
2024-11-12 19:42:07 +0000

    add credit method to api

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -134,3 +134,21 @@ export const getNowPlaying = () => {
             throw error
         });
 };
+
+export const getMovieCredits = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then((response) => {
+        if (!response.ok) {
+            return response.json().then((error) => {
+                throw new Error(error.status_message || "Something went wrong");
+            });
+        }
+        return response.json();
+    })
+        .catch((error) => {
+            throw error
+        });
+};
